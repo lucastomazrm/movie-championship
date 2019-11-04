17ae3acca2c5adc82c82c9f155da8b987d414f46
Lucas Tomaz Resende Miranda <lucas.rmiranda@ibm.com>
2019-11-03 21:05:28 -0300

    semifinal api

--- server/routers/movies.js
+++ server/routers/movies.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const fetch = require("node-fetch");
 
-module.exports = function(app) {
+module.exports = function (app) {
   const router = express.Router();
 
-  router.get("/", function(req, res, next) {
+  router.get("/", function (req, res, next) {
     const url = "https://copafilmes.azurewebsites.net/api/filmes";
 
     const requestInfo = {
@@ -37,7 +37,7 @@ module.exports = function(app) {
       });
   });
 
-  router.post("/groups", function(req, res, next) {
+  router.post("/groups", function (req, res, next) {
     const movies = req.body;
 
     const sortedMovies = movies.sort((a, b) => {
@@ -56,6 +56,41 @@ module.exports = function(app) {
       });
     }
     res.send(groups);
+  });
+
+  router.post("/groups/seminfinal", function (req, res, next) {
+    const movies = req.body;
+
+    let semifinal = [{ movies: [] }, { movies: [] }];
+    movies.map((group, index) => {
+      let winner;
+      if (group.movies[0].score === group.movies[1].score) {
+        winner = group.movies.sort((a, b) => {
+          if (a.title < b.title) {
+            return -1;
+          }
+          if (a.title > b.title) {
+            return 1;
+          }
+          return 0;
+        })[0];
+      } else {
+        winner = group.movies.sort((m1, m2) => m2.score - m1.score)[0];
+      }
+      switch (index) {
+        case 0: semifinal[0].movies.push(winner);
+          break;
+        case 1: semifinal[1].movies.push(winner);
+          break;
+        case 2: semifinal[1].movies.push(winner);
+          break;
+        case 3: semifinal[0].movies.push(winner);
+          break;
+        default: break;
+      }
+    });
+    res.send(semifinal);
+
   });
 
   app.use("/movies", router);
